6977860ef46029b9c9f410a8c466e10fda11de43
Maneren <49210777+Maneren@users.noreply.github.com>
2021-08-20 23:17:59 +0200

    added cli

--- app.js
+++ app.js
@@ -2,32 +2,40 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 
-const fileUpload = require('express-fileupload');
-const cors = require('cors');
-const morgan = require('morgan');
+const { ArgumentParser } = require('argparse');
+const parser = new ArgumentParser({
+  description: 'Simple file sharing server'
+});
+
+parser.add_argument('-d', '--dev', { action: 'store_true', help: 'run in dev mode' });
+parser.add_argument('-q', '--qr', { action: 'store_true', help: 'show qr code' });
+parser.add_argument('-p', '--port', { type: 'int' });
+parser.add_argument('folder', { nargs: '?', default: path.join(__dirname, 'shared'), help: 'shared folder' });
 
 const app = express();
+const argv = parser.parse_args();
 
+const sharedFolder = argv.folder;
 const buildFolder = path.join(__dirname, 'client', 'build');
-const sharedFolder = path.join(__dirname, 'shared');
 
-if (!fs.existsSync(buildFolder)) {
-  fs.mkdirSync(buildFolder);
+if (!fs.existsSync(sharedFolder)) {
+  fs.mkdirSync(sharedFolder);
+}
+
+if (!fs.existsSync(buildFolder) || !fs.readdirSync(buildFolder).includes('index.html')) {
+  console.log('Couldn\'t find index.html. Try rebuilding the client');
+  require('process').exit(1);
 }
 
 app.use('/', express.static(buildFolder));
 app.use('/files', express.static(sharedFolder));
 
-// enable upload
-app.use(fileUpload({ createParentPath: true }));
+app.use(require('express-fileupload')());
+app.use(require('cors')());
+app.use(require('morgan')('dev'));
 
-// other middleware
-app.use(cors());
-app.use(morgan('dev'));
-
-app.post('/upload', (req, res) => {
+app.post('/upload', ({ files }, res) => {
   try {
-    const { files } = req;
     if (!files?.uploads) {
       return res.status(400).redirect(`/?error=${encodeURIComponent('No files selected')}`);
     }
@@ -53,14 +61,25 @@ app.get('/file-list', (req, res, next) => {
   }
 });
 
-const server = app.listen(0, () => {
-  const qrcode = require('qrcode-terminal');
-  const ip = require('ip');
+console.log(`Using folder: ${sharedFolder}`);
 
-  const port = server.address().port;
-  const address = `http://${ip.address()}:${port}`;
-  qrcode.generate(address, { small: true });
-  console.log(`Listening at ${address}`);
-});
+if (argv.dev) {
+  const port = 5000;
+  app.listen(port, () => console.log(`Listening at http://localhost:${port}`));
+} else {
+  let port = 0;
+  if (argv.port) {
+    port = argv.port;
+  }
 
-// TODO: add cli
+  const server = app.listen(port, () => {
+    const { port } = server.address();
+    const ip = require('ip').address('public');
+
+    const address = `http://${ip}:${port}`;
+
+    require('qrcode-terminal').generate(address, { small: true });
+
+    console.log(`Listening at ${address}`);
+  });
+}
